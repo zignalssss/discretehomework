a6da221bc181fecd8aef77034e336dc9904844bf
Wiritipon Thapkhan <81100736+zignalssss@users.noreply.github.com>
2023-10-07 01:01:21 +0700

    Update modular_division.py

--- Python/modular_division.py
+++ Python/modular_division.py
@@ -1,33 +1,16 @@
-import math
-def primetest(num):
-    if(num <= 1):
-        return False
-    for i in range(2,int(math.sqrt(num))+1):
-        if(num % i == 0):
-            return False
-    return True
-def InverseFermat(b,n):
-    if ((b*pow(b,(n-2))) % n == 1):
-        return pow(b,(n-2))
-def InverseMulOfDivision(b , n):
-    for i in range(1,n):
-        if((4*i)%9 == 1):
-            return i
-    return -1
-def checkAnswerLength(number , n):
-      if(number < n):
-        return number
-      else:
-        return checkAnswerLength(number%n,n)
+def Ex_GCD(a ,b):
+   if b == 0:
+      return (a,1,0)
+   g , s_prime , t_prime = Ex_GCD(b,a%b)
+   s = t_prime
+   t = s_prime - (a//b)*t_prime
+   return (g,s,t)
 a , b , n = input().split()
 a = int(a)
 b = int(b)
 n = int(n)
-if (primetest(n)):
-    if n % b != 0:
-       print(checkAnswerLength(InverseFermat(b,n) , n)*a)
-    else:
-       print(checkAnswerLength(InverseMulOfDivision(b,n)*a , n))
-else:
-    print(checkAnswerLength(InverseMulOfDivision(b,n)*a , n))
+gcd , s_gcd , t_gcd = Ex_GCD(n,b)
+print(a*t_gcd%n)
+
+
 
